04c912d99459da0e35324a2a99b86a39a9120075
lahmidi <dvabderrahman@gmail.com>
2025-01-28 13:35:46 +0100

    create the translation in the search bar

--- Client/src/sections/homeSections/Hero.jsx
+++ Client/src/sections/homeSections/Hero.jsx
@@ -16,54 +16,38 @@ export default function Hero() {
           {t('hero.description')}
         </p>
         <form className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 bg-white rounded p-[10px] w-full">
-          <select
-            id="city"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          >
-            <option selected>city</option>
-            <option value="US">United States</option>
-            <option value="CA">Canada</option>
-            <option value="FR">France</option>
-            <option value="DE">Germany</option>
-          </select>
-
-          <select
-            id="property-type"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          >
-            <option selected>property type</option>
-            <option value="apartment">Apartment</option>
-            <option value="house">House</option>
-            <option value="condo">Condo</option>
-            <option value="villa">Villa</option>
-          </select>
-
-          <select
-            id="price-range"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          >
-            <option selected>price range</option>
-            <option value="1000-2000">$1000 - $2000</option>
-            <option value="2000-3000">$2000 - $3000</option>
-            <option value="3000-4000">$3000 - $4000</option>
-            <option value="4000-5000">$4000 - $5000</option>
-          </select>
-
-          <select
-            id="rooms"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          >
-            <option selected>rooms</option>
-            <option value="1">1 Room</option>
-            <option value="2">2 Rooms</option>
-            <option value="3">3 Rooms</option>
-            <option value="4">4 Rooms</option>
-            <option value="5+">5+ Rooms</option>
-          </select>
-
+           <select id="city" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+             <option selected>{t('form.city.label')}</option>
+             <option value="US">{t('form.city.options.US')}</option>
+             <option value="CA">{t('form.city.options.CA')}</option>
+             <option value="FR">{t('form.city.options.FR')}</option>
+             <option value="DE">{t('form.city.options.DE')}</option>
+           </select>
+            <select id="property-type" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+              <option selected>{t('form.propertyType.label')}</option>
+              <option value="apartment">{t('form.propertyType.options.apartment')}</option>
+              <option value="house">{t('form.propertyType.options.house')}</option>
+              <option value="condo">{t('form.propertyType.options.condo')}</option>
+              <option value="villa">{t('form.propertyType.options.villa')}</option>
+            </select>
+            <select id="price-range" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+              <option selected>{t('form.priceRange.label')}</option>
+              <option value="1000-2000">{t('form.priceRange.options.1000-2000')}</option>
+              <option value="2000-3000">{t('form.priceRange.options.2000-3000')}</option>
+              <option value="3000-4000">{t('form.priceRange.options.3000-4000')}</option>
+              <option value="4000-5000">{t('form.priceRange.options.4000-5000')}</option>
+            </select>
+            <select id="rooms" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+              <option selected>{t('form.rooms.label')}</option>
+              <option value="1">{t('form.rooms.options.1')}</option>
+              <option value="2">{t('form.rooms.options.2')}</option>
+              <option value="3">{t('form.rooms.options.3')}</option>
+              <option value="4">{t('form.rooms.options.4')}</option>
+              <option value="5+">{t('form.rooms.options.5+')}</option>
+            </select>
           <button
             type="submit"
-            className="inline-flex items-center py-2.5 px-3 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+            className="inline-flex cursor-pointer items-center py-2.5 px-3 text-sm font-medium text-white bg-blue-600 rounded-lg border border-blue-700 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300"
           >
             <svg
               className="w-4 h-4 me-2"
@@ -80,7 +64,7 @@ export default function Hero() {
                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
               />
             </svg>
-            Search
+            {t('buttons.search')}
           </button>
         </form>
       </div>
